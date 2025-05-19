4c34c9c42beef5e42a854c424c0d3b26417b2d10
MyriamKuhn <myriam.kuehn@free.fr>
2025-05-19 21:25:11 +0200

    Production: only build output

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('affiche le titre Vite + React', () => {
-  render(<App />)
-  // on recherche maintenant "Vite + React"
-  expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument()
-})
